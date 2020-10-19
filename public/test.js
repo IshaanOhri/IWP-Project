5419c2df3154ac2c0655620a81fcfa9888a7479a
MitushiRaj <mitushiraj170@gmail.com>
2020-10-19 19:29:17 +0530

    added js

---
+++ public/test.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $("#btnLogin").click(function (e) {
+        e.preventDefault();
+        var email = $('#email').val();
+        var password = $('#password').val();
+        var repassword = $('#repassword').val();
+        var settings = {
+            "url": "https://iwp-project.ml/login",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({ "email": "ishaan99ohri@gmail.com", "password": "hello123" }),
+        };
+        $(".error1").remove();
+        if (email.length < 1) {
+            $('#email').after('<span class="error">This field is required</span>');
+        } else {
+            var regEx = /^[a-z0-9._-]+@[gmail]+.[com]{2,5}$/i;
+            var validEmail = regEx.test(email);
+            if (!validEmail) {
+                $('#email').after('<span class="error">Enter a valid email</span>');
+            }
+        }
+        if (password.length < 8) {
+            $('#password').after('<span class="error">Password must be at least 8 characters long</span>');
+        }
+        if (password != repassword) {
+            $('#repassword').after('<span class="error">Password does not match </span>')
+        }
+        else {
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+                window.open("index.html", "_self");
+
+            });
+        }
+    });
+});
+
+
